e5985af3c257a0b4c84aaf51ff7cafe751421c2a
leonawicz <matt.rstat@gmail.com>
2018-05-17 22:00:21 -0600

    Add data.

---
+++ R/trekfont.R
@@ -0,0 +1,6 @@
+#' Available Star Trek fonts.
+#'
+#' Comprehensive listing of all available fonts. These fonts are sourced from https://github.com/wrstone/fonts-startrek under the GPL-3 license.
+#'
+#' @format A character vector of 107 font files.
+"trekfonts"
